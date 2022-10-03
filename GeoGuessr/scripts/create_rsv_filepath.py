765c121df03dad5e1a60cffdd26d00c7d0717314
David <david@Sega.local>
2022-10-03 11:16:32 +0200

    add missing directories

--- GeoGuessr/scripts/create_rsv_filepath.py
+++ GeoGuessr/scripts/create_rsv_filepath.py
@@ -4,8 +4,13 @@ import os
 import json
 import pandas as pd 
 
-FOLDER_NAME = 'rsv'
-for k in ['indv', 'pano']:
-    os.mkdir(f'../data/{FOLDER_NAME}_{k}')
-    os.mkdir(f'../data/{FOLDER_NAME}_{k}/train')
-    os.mkdir(f'../data/{FOLDER_NAME}_{k}/unlabeled')
+from pathlib import Path
+
+parent_dir = Path(__file__).parent.parent
+folder_names = ['rsv', 'rsv2']
+
+for folder_name in folder_names:
+    for k in ['indv', 'pano']:
+        Path.mkdir(parent_dir / 'data' / f'{folder_name}_{k}', exist_ok=True)
+        Path.mkdir(parent_dir / 'data' / f'{folder_name}_{k}' / 'train', exist_ok=True)
+        Path.mkdir(parent_dir / 'data' / f'{folder_name}_{k}' / 'unlabeled', exist_ok=True)
